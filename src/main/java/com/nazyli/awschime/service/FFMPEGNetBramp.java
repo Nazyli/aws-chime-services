40f7a92e7b652190907ddd1a80f33a89c8c72305
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-03-11 16:53:59 +0700

    add check job state finished

--- src/main/java/com/nazyli/awschime/service/FFMPEGNetBramp.java
+++ src/main/java/com/nazyli/awschime/service/FFMPEGNetBramp.java
@@ -3,13 +3,14 @@ package com.nazyli.awschime.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.google.common.collect.Lists;
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -23,68 +24,74 @@ import java.util.stream.Collectors;
 
 @Component
 public class FFMPEGNetBramp {
-
     @Value("${aws.bucket.name}")
     private String bucketName;
 
-    private static final String FOLDER_MEETING = "captures/%s/";
-    private static final String TEMPORARY_SAVE = "/Users/nazyli/downloads/v-call/" + FOLDER_MEETING;
-    private static final String AUDIO = "audio";
-    private static final String AUDIO_NAME = "processedAudio.mp4";
-
     private final AmazonS3 s3Client;
+//    private final FFmpeg ffmpeg = new FFmpeg("/usr/local/bin/ffmpeg");
+    private final FFmpeg ffmpeg = new FFmpeg();
+    private final FFprobe ffprobe = new FFprobe();
+    private static final String FOLDER_MEETING = "captures/%s/";
+    private static final String TEMPORARY_SAVE = "/tmp/" + FOLDER_MEETING;
 
-    public FFMPEGNetBramp(AmazonS3 s3Client) {
+    public FFMPEGNetBramp(AmazonS3 s3Client) throws IOException {
         this.s3Client = s3Client;
     }
 
-
-    public PutObjectResult AudioProcess(String meetingId) throws IOException {
+    public Object AudioCompositing(String meetingId) throws IOException {
+        String audio = "audio";
+        String audioName = audio + "_" + meetingId + ".mp4";
         String outputFile = String.format(TEMPORARY_SAVE, meetingId);
-        String outputFileAudio = outputFile + AUDIO_NAME;
+        String outputFileAudio = outputFile + audioName;
 
-        List<String> path = listMediaUrl(meetingId, AUDIO).stream()
+        List<String> pathList = listMediaUrl(meetingId, audio).stream()
                 .map(e -> s3Client.getUrl(bucketName, e).toExternalForm())
                 .collect(Collectors.toList());
 
-        try {
-            final FFmpeg ffmpeg = new FFmpeg("/usr/local/bin/ffmpeg");
-            final FFprobe ffprobe = new FFprobe("/usr/local/bin/ffprobe");
-
-            String filesStrings = Lists.newArrayList(path)
-                    .stream()
-                    .map(p -> "file '" + p + "'")
-                    .collect(Collectors.joining(System.getProperty("line.separator")));
-
-            Path listOfFiles = Files.createTempFile(Path.of(outputFile), "ffmpeg-audio-", ".txt");
-            Files.write(listOfFiles, filesStrings.getBytes());
-
-            FFmpegBuilder builder = new FFmpegBuilder()
-                    .setInput(listOfFiles.toAbsolutePath().toString())
-                    .addExtraArgs("-f", "concat", "-safe", "0", "-protocol_whitelist", "file,https,tls,tcp")
-                    .setFormat("mp4")
-                    .addOutput(outputFileAudio)
-                    .addExtraArgs("-c", "copy")
-                    .setAudioCodec("aac")
-                    .setVideoCodec("libx264")
-                    .done();
-
-            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-            executor.createJob(builder).run();
-
-            return upload(meetingId, outputFileAudio);
-        } catch (IOException e) {
-            throw new IOException(e);
+        File theDir = new File(outputFile);
+        boolean checkDir = theDir.exists();
+        if (!checkDir && !pathList.isEmpty()) {
+            checkDir = theDir.mkdirs();
         }
+        if (checkDir) {
+            try {
+                String filesStrings = Lists.newArrayList(pathList)
+                        .stream()
+                        .map(p -> "file '" + p + "'")
+                        .collect(Collectors.joining(System.getProperty("line.separator")));
+
+                Path listOfFiles = Files.createTempFile(Path.of(outputFile), "ffmpeg-audio-", ".txt");
+                Files.write(listOfFiles, filesStrings.getBytes());
+
+                FFmpegBuilder builder = new FFmpegBuilder()
+                        .setInput(listOfFiles.toAbsolutePath().toString())
+                        .addExtraArgs("-f", "concat", "-safe", "0", "-protocol_whitelist", "file,https,tls,tcp")
+                        .setFormat("mp4")
+                        .addOutput(outputFileAudio)
+                        .addExtraArgs("-c", "copy")
+                        .setAudioCodec("aac")
+                        .setVideoCodec("libx264")
+                        .done();
+                FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+                FFmpegJob job = executor.createJob(builder);
+                job.run();
+                if (FFmpegJob.State.FINISHED == job.getState()) {
+                    String key = String.format(FOLDER_MEETING, meetingId) + audioName;
+                    s3Client.putObject(bucketName, key, new File(outputFileAudio));
+                    FileUtils.deleteDirectory(new File(outputFile));
+
+                    return s3Client.getUrl(bucketName, key).toExternalForm();
+                }
+                return null;
+            } catch (IOException e) {
+                throw new IOException(e);
+            }
+        }
+        return null;
     }
 
-    public PutObjectResult upload(String meetingId, String fileTmp) {
-        String key = String.format(FOLDER_MEETING, meetingId) + AUDIO_NAME;
-        return s3Client.putObject(bucketName, key, new File(fileTmp));
-    }
-
-    public List<String> listMediaUrl(String meetingId, String media) {
-        String prefixBucket = String.format(FOLDER_MEETING, meetingId) + media;
+    public List<String> listMediaUrl(String meetingId, String folder) {
+        String prefixBucket = String.format(FOLDER_MEETING, meetingId) + folder;
         ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
                 .withBucketName(bucketName).withPrefix(prefixBucket);
         ObjectListing objects = s3Client.listObjects(listObjectsRequest);
@@ -93,5 +100,4 @@ public class FFMPEGNetBramp {
                 .sorted(Comparator.comparing(S3ObjectSummary::getLastModified))
                 .map(S3ObjectSummary::getKey).collect(Collectors.toList());
     }
-
 }
